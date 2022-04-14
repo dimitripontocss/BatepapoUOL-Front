273d9bcacde20029c6c917a7fa97425f909839f1
Dimitri <dimitri@gmail.com>
2022-04-14 00:40:03 -0300

    Quarta versão - Projeto pronto sidebar quase pronta e faltam dois bônus

--- scripts.js
+++ scripts.js
@@ -29,7 +29,7 @@ function renderizarMSG(elemento){
     chat.innerHTML = "";
     for(let i = 0; i < auxiliar.length; i++){
         if(auxiliar[i].type == "private_message"){
-            if(auxiliar[i].to == "Todos" || auxiliar[i].to == nome){
+            if(auxiliar[i].to == "Todos" || auxiliar[i].to == nome || auxiliar[i].from == nome){
                 chat.innerHTML += `
             <div class="${auxiliar[i].type}">
                 <span class="hora">(${auxiliar[i].time})</span> <span><strong>${auxiliar[i].from}</strong> para <strong>${auxiliar[i].to}</strong>: ${auxiliar[i].text}</span>
@@ -42,10 +42,59 @@ function renderizarMSG(elemento){
             </div>
             `}
         }
+        const aux = chat.querySelector(":nth-child(99)");
+        aux.scrollIntoView();
     }
+
     
 
 setInterval(capturaMSG, 3000);
+
+
+
+
+function buscaAtivos(){
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
+    promise.then(inserirAtivos);
+}
+
+buscaAtivos();
+
+function inserirAtivos(elemento){
+    const auxiliar = elemento.data;
+    const aux = document.querySelector(".ativos");
+    aux.innerHTML = "";
+    aux.innerHTML = `<div onclick="destino(this)"><ion-icon name="people"></ion-icon> <span>Todos</span></div>`;
+    for(let i = 0; i<auxiliar.length; i++){
+        aux.innerHTML += `
+        <div onclick="destino(this)"><ion-icon name="people"></ion-icon> <span>${auxiliar[i].name}</span></div>
+        `
+    }
+}
+
+setInterval(buscaAtivos, 10000);
+
+function  ativaSubtxt(){
+
+}
+
+function destino(elemento){
+    destinatario =  elemento.innerText;
+    console.log(destinatario);
+    ativaSubtxt();
+}
+
+function qualTipo(elemento){
+    const auxiliar = elemento.innerText;
+    console.log(tipoMSG);
+    if(auxiliar == "Reservadamente"){
+        tipoMSG = "private_message";
+    }else{
+        tipoMSG = "message";
+    }
+    console.log(tipoMSG);
+}
+
 
 function enviarMSG(){
     let msg = document.querySelector(".inputChat").value;
@@ -66,6 +115,7 @@ function enviarMSG(){
 }
 
 function enviou(){
+    
     capturaMSG();
 }
 
